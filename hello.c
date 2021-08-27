3bab55cd0bccb498de6b6d85277d314af8ebff01
Winston <wasaow@outlook.com>
2021-08-27 22:21:07 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,18 +1,18 @@
 /**
  * Author: Winston
  * Date: 2021
- * 
+ *
  * Hello world program
  */
- 
- 
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
 
-    printf("Winston\n");
+    printf("Winston!!!\nComputer Science\n");
 
     return 0;
 }
