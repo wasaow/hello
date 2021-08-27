7b732f61fe274335e7a1c4429b3ee4949e12a056
Winston <wasaow@outlook.com>
2021-08-27 21:03:15 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Winston
+ * Date: 2021
+ * 
+ * Hello world program
+ */
+ 
+ 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+    printf("Winston\n");
+
+    return 0;
+}
